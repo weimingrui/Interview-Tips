c010564616e74c772c4e6d77e85ffeefbaf01030
Arthur <1140735765@qq.com>
2021-03-07 23:02:17 +0800

    promise实现调度器

---
+++ promise调度.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: Arthur
+ * @Date: 2021-03-07 22:49:06
+ * @LastEditors: Arthur
+ * @LastEditTime: 2021-03-07 23:01:56
+ * @Description: file content
+ */
+class Scheduler {
+    constructor(maxNum) {
+        this.taskList = [];
+        this.counter = 0;
+        this.maxNum = maxNum;
+    }
+    async add(promiseCreater) {
+        if (this.counter >= this.maxNum) {
+            await new Promise((resolve,reject)=>{
+                this.taskList.push(resolve);
+            })
+        }
+        this.counter ++;
+        const result = await promiseCreater();
+        this.counter--;
+        if (this.taskList.length) {
+            this.taskList.shift();
+        }
+        return result;
+    }
+}
